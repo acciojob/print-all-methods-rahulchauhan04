63d668c302ac0e331f36060aa7974296203084db
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-03-01 22:42:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//your JS code here. If required.
 function allMethods() {
-  //write your code here
+  return Object.getOwnPropertyNames(Math).filter(function (property) {
+    return typeof Math[property] === 'function';
+  }).join(", ");
 }
 
+// Showing the methods in an alert
 alert(allMethods());
